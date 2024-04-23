ca97476b4d39c99b7ede75f98bed07704623b38a
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-23 22:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const container=document.querySelector('.container');
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
+}
