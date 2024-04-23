fbd54018731f47164d23ef050412819b7d804f39
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-23 22:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-//your JS code here. If required.
-const container = document.querySelector('.container');
 
-for (let i = 0; i < 800; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
+// const container = document.querySelector('.container');;
+// for (let i = 0; i < 800; i++) {
+//   const square = document.createElement('div');
+//   square.classList.add('square');
+window.onload = function() {
+  const container = document.querySelector('.container');
+  for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+}
   
   square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'randomColor()'; // replace randomColor() with a function that generates random colors
+    square.style.backgroundColor = 'randomColor()';
   });
   
   square.addEventListener('mouseout', () => {
