da580215b7c72c9d723df8d46c7c82a9dcc28c4e
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-05-01 20:15:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+// window.onload = function() {
+//   const container = document.querySelector('.container');
+//   for (let i = 0; i < 800; i++) {
+//     const square = document.createElement('div');
+//     square.classList.add('square');
+    
+//     square.addEventListener('mouseover', () => {
+//       square.style.backgroundColor = randomColor();
+//     });
+  
+//     square.addEventListener('mouseout', () => {
+//       setTimeout(() => {
+//         square.style.backgroundColor = 'initial';
+//       }, 1000);
+//     });
+    
+//     container.appendChild(square);
+//   }
+// }
 
-// const container = document.querySelector('.container');;
-// for (let i = 0; i < 800; i++) {
-//   const square = document.createElement('div');
-//   square.classList.add('square');
+// function randomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
 window.onload = function() {
   const container = document.querySelector('.container');
   for (let i = 0; i < 800; i++) {
@@ -11,12 +34,6 @@ window.onload = function() {
     
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = randomColor();
-    });
-  
-    square.addEventListener('mouseout', () => {
-      setTimeout(() => {
-        square.style.backgroundColor = 'initial';
-      }, 1000);
     });
     
     container.appendChild(square);
