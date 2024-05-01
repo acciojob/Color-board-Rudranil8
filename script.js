f3cbcaf3ae98934ce14691a650be1e9b75bc1617
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-05-01 19:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,19 +8,26 @@ window.onload = function() {
   for (let i = 0; i < 800; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = randomColor();
+    });
+  
+    square.addEventListener('mouseout', () => {
+      setTimeout(() => {
+        square.style.backgroundColor = 'initial';
+      }, 1000);
+    });
+    
     container.appendChild(square);
   }
 }
-  
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'randomColor()';
-  });
-  
-  square.addEventListener('mouseout', () => {
-    setTimeout(() => {
-      square.style.backgroundColor = 'initial';
-    }, 1000);
-  });
-  
-  container.appendChild(square);
+
+function randomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
